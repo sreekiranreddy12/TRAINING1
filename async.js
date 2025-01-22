255f5f4b8959e295c759c87d583bcd34e0b82131
sreekiranreddy12 <sreekiran122004@gmail.com>
2025-01-22 21:44:30 +0530

    Add files via upload

--- async.js
+++ async.js
@@ -11,19 +11,49 @@
 // const { reject } = require("async");
 
 // console.log(50);
-let p1 = new Promise((reslove, reject)=>{
+// let p1 = new Promise((reslove, reject)=>{
    
-});
-console.log(p1);
-let p2 = new Promise((reslove, reject)=>{
-   reslove("success");
-});
-p2.then((response)=>{console.log(response)});
-p2.catch((error)=> {console.log(error)} );
-p2.finally(() => {console.log("finally for both ")});
+// });
+// console.log(p1);
+// let p2 = new Promise((reslove, reject)=>{
+//    reslove("success");
+// });
+// p2.then((response)=>{console.log(response)});
+// p2.catch((error)=> {console.log(error)} );
+// p2.finally(() => {console.log("finally for both ")});
 
 // let p3 = new Promise((reslove, reject)=>{
 //     reject("success");
 //     p2.catch(()=>{} );
 //  });
 //  console.log(p3);
+//!  API fetching
+function fetchusers(){
+   let response= fetch("https://jsonplaceholder.typicode.com/users");
+   response.then(result=>{
+
+       //return result.json().//then (data=>{
+          
+         return result.json().then(data=>{
+            console.log(data);
+            let user=document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+               store.innerHTML +=`
+            <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+
+            <td>${user.email}</td>
+
+            <td>${user.company.name}</td>
+
+            </tr>
+               `
+            })
+         });
+   //    })
+   })
+   .catch(err=>console.log(err))
+}
+fetchusers();
